0da34a9fc60773c6e6a303d89a0c38c57a9ca592
Dan McKeown <danomick@gmail.com>
2017-03-06 02:17:49 -0800

    Update regression.py

--- regression.py
+++ regression.py
@@ -86,19 +86,7 @@ print progenitor
 
 #### Redshift File
 
-
-
-red = open( "redshift.txt")
-
-reds =  [elem.strip().split(';') for elem in red]
-
-redder = list(reds)
-redd = [val for sublist in redder for val in sublist]
-redshift = [float(i) for i in redd]
-print "now printing redshift"
-print redshift
-
-
+redshift = 2.2204460492503099e-16	
 
 
 ##### Halo File
@@ -226,7 +214,7 @@ while nm < g:
 
 # Here the data is inserted into the data frame,halo_id[nm],dm_mass[nm],redshift[nm],descendants[nm],progenitor[nm],parenthalos[nm]]
 
-	df.loc[nm] = [nm,halo_id[nm], popt[2] ,pcov[2,2]**0.5,popt[0],pcov[0,0]**0.5,popt[1],pcov[1,1]**0.5,dm_mass[nm],progenitor[nm],parenthalos[nm],redshift[nm],chi_squared,dark_mass_percent[nm]]
+	df.loc[nm] = [nm,halo_id[nm], popt[2] ,pcov[2,2]**0.5,popt[0],pcov[0,0]**0.5,popt[1],pcov[1,1]**0.5,dm_mass[nm],progenitor[nm],parenthalos[nm],redshift,chi_squared,dark_mass_percent[nm]]
 	#df = df.round({'n': 7, 'nerror': 5, 'a':5 ,'aerror':5,'r0':5,'r0error':5,'dm halo mass':6,'progenitor':1,'parent':1,'redshift':5,'chi_square':4,'mass_percent':4})
 
 	
@@ -249,7 +237,7 @@ while nm < g:
 	ax2.plot(r3,total_dm_mass, 'r.')
 	ax2.set_ylabel('Dark Matter Mass 10^10 Msolar', color='r')
 	ax2.tick_params('y', colors='r')	
-	plt.title('Dark Matter Density of halo %s at Redshift  %s'%(halo_id[nm],redshift[nm]))
+	plt.title('Dark Matter Density of halo %s at Redshift  %s'%(halo_id[nm],redshift))
 
 	
 	
@@ -259,42 +247,8 @@ while nm < g:
 
 
 ########  Create data table 
-print "now printing data frame"
-print df
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.externals import six
 
 
-
-def render_mpl_table(data, col_width=0.70, row_height=0.225, font_size=12,
-                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
-                     bbox=[0, 0, 1, 1], header_columns=0,
-                     ax=None, **kwargs):
-    if ax is None:
-        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
-        fig, ax = plt.subplots(figsize=size)
-        ax.axis('off')
-
-    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
-
-    mpl_table.auto_set_font_size(False)
-    mpl_table.set_fontsize(font_size)
-
-    for k, cell in six.iteritems(mpl_table._cells):
-        cell.set_edgecolor(edge_color)
-        if k[0] == 0 or k[1] < header_columns:
-            cell.set_text_props(weight='bold', color='w')
-            cell.set_facecolor(header_color)
-        else:
-            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
-    return ax
-
-
-render_mpl_table(df, header_columns=0, col_width=2.0)
-
-plt.savefig('DMHaloinfo135.png')
 
 ####### End of data table created. Now close pdf pages.
 
@@ -308,7 +262,7 @@ conn = sqlite3.connect('darkmatter2.db')    #Here I will create a database of th
 c = conn.cursor()   #my cursor
 
 # Create table
-c.execute('''CREATE TABLE Dlow
+c.execute('''CREATE TABLE D1low
              (number int PRIMARY KEY, id int , n float, ner float, a float, aer float, r0 float, r0er float,dm_mass float, progenitor int, parent int,redshift float,chi_squared float, dm_mass_percent float)''')
 
 
@@ -323,7 +277,7 @@ print list_of_darkhalos
 for x in list_of_darkhalos:
 	
 	halo_string = ', '.join('?' * len(x))
-	query_string = 'INSERT INTO Dlow VALUES (%s);' % halo_string
+	query_string = 'INSERT INTO D1low VALUES (%s);' % halo_string
 	c.execute(query_string, x)
 	
 	
